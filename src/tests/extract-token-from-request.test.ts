c6e4ce082477dbdd6a76d33d6c911b028f505862
TuvalSimha <tuval.simha@gmail.com>
2022-10-06 15:54:20 +0300

    Fix

--- src/tests/extract-token-from-request.test.ts
+++ src/tests/extract-token-from-request.test.ts
@@ -2,9 +2,15 @@ import { describe, expect, test } from "@jest/globals";
 import { validateAndExtractTokenFromRequest } from "../utils/validate-and-extract-token-from-request";
 
 describe("extract-token-from-request", () => {
-  test("When header is null -> throw error status 403", () => {
-    
-    const result = validateAndExtractTokenFromRequest(request);
-    expect(result).toThrowError("Authader is missing");
+  test("When header is null -> throw error status 403", async () => {
+    const request = new Request("http://www.example.com", {
+      headers: {
+        keys: "authorization",
+        values: "Bearer 1234567890",
+      },
+    });
+    console.log(request);
+    const result = await validateAndExtractTokenFromRequest(request);
+    expect(result).toThrow(Error);
   });
 });
