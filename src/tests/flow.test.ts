8a727384591e770fcf1aaaa83f3579208cee79a9
TuvalSimha <tuval.simha@gmail.com>
2022-10-10 09:28:56 +0300

    Add tests to flow

--- src/tests/flow.test.ts
+++ src/tests/flow.test.ts
@@ -1,8 +1,10 @@
-import { describe, expect, test } from "@jest/globals";
+import { afterEach, beforeEach, describe, expect, test } from "@jest/globals";
 import { handleRequest } from "../utils/flow";
 import { sign } from "@tsndr/cloudflare-worker-jwt";
+import mockFetch from "jest-mock-fetch";
+import fetch from "jest-mock-fetch";
 
-async function buildValidRequest(timestamp: number) {
+async function buildValidRequest(timestamp: number | string | null) {
   const jwtVerifySecret = "fake";
   const validToken = await sign({}, jwtVerifySecret);
   const request = new Request(`https://fake.com/?timestamp=${timestamp}`, {
@@ -18,10 +20,103 @@ async function buildValidRequest(timestamp: number) {
 }
 
 describe("Request/Response flow", () => {
+  beforeEach(() => {
+    fetch.reset();
+  });
   test.only("Should return a valid response when timestamp param is valid", async () => {
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
+      {
+        data: {
+          blocks: [
+            {
+              id: "0xd125365fb2839beefb583319429a544da58758548a0454fddb9fed553ed94b06",
+              number: "15653542",
+              timestamp: "1664630075",
+            },
+          ],
+        },
+      }
+    );
+    expect(fetch.mock.calls[0][1]).toEqual(
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
+    );
     const { request, jwtVerifySecret } = await buildValidRequest(1665295732);
-    const response = await handleRequest(request, { jwtVerifySecret });
 
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/juanmardefago/grt-circulating-supply",
+      {
+        data: {
+          globalStates: [
+            {
+              totalSupply: "10472593084278602817126230051",
+              lockedSupply: "2481516171545208333333335778",
+              lockedSupplyGenesis: "2406735547043125000000001983",
+              liquidSupply: "7991076912733394483792894273",
+              circulatingSupply: "8065857537235477817126228068",
+            },
+          ],
+        },
+      }
+    );
+
+    const response = await handleRequest(request, { jwtVerifySecret });
+    expect(fetch.mock.calls.length).toEqual(2);
+    expect(response.headers.get("Content-Type")).toBe("application/json");
+    expect(response.status).toBe(200);
+    expect(await response.json()).toEqual(
+      expect.objectContaining({
+        totalSupply: expect.any(String),
+        lockedSupply: expect.any(String),
+        lockedSupplyGenesis: expect.any(String),
+        liquidSupply: expect.any(String),
+        circulatingSupply: expect.any(String),
+      })
+    );
+  });
+  test.only("When Timestamp is Null -> Should return lastGlobalState values", async () => {
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
+      {
+        data: {
+          blocks: [
+            {
+              id: "0xd125365fb2839beefb583319429a544da58758548a0454fddb9fed553ed94b06",
+              number: "15653542",
+              timestamp: "1664630075",
+            },
+          ],
+        },
+      }
+    );
+    expect(fetch.mock.calls[0][1]).toEqual(
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
+    );
+    const { request, jwtVerifySecret } = await buildValidRequest(null);
+
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/juanmardefago/grt-circulating-supply",
+      {
+        data: {
+          globalStates: [
+            {
+              totalSupply: "10472593084278602817126230051",
+              lockedSupply: "2481516171545208333333335778",
+              lockedSupplyGenesis: "2406735547043125000000001983",
+              liquidSupply: "7991076912733394483792894273",
+              circulatingSupply: "8065857537235477817126228068",
+            },
+          ],
+        },
+      }
+    );
+
+    const response = await handleRequest(request, { jwtVerifySecret });
+    expect(fetch.mock.calls.length).toEqual(4);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
@@ -35,16 +130,57 @@ describe("Request/Response flow", () => {
     );
   });
 
-  // test("When Timestamp is Null -> Should return lastGlobalState values", async () => {
-  //   const result = await flow();
-  //   expect(result).not.toBeNull();
-  //   expect(result).toContain("lastGlobalState");
-  // });
+  test.only("When Timestamp is String -> Should return lastGlobalState values", async () => {
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
+      {
+        data: {
+          blocks: [
+            {
+              id: "0xd125365fb2839beefb583319429a544da58758548a0454fddb9fed553ed94b06",
+              number: "15653542",
+              timestamp: "1664630075",
+            },
+          ],
+        },
+      }
+    );
+    expect(fetch.mock.calls[0][1]).toEqual(
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
+    );
+    const { request, jwtVerifySecret } = await buildValidRequest("boop");
 
-  // test("When Timestamp is String -> Should return lastGlobalState values", async () => {
-  //   const timestamp = "boop";
-  //   // @ts-expect-error
-  //   const result = await flow(timestamp);
-  //   expect(result).toThrowError();
-  // });
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/juanmardefago/grt-circulating-supply",
+      {
+        data: {
+          globalStates: [
+            {
+              totalSupply: "10472593084278602817126230051",
+              lockedSupply: "2481516171545208333333335778",
+              lockedSupplyGenesis: "2406735547043125000000001983",
+              liquidSupply: "7991076912733394483792894273",
+              circulatingSupply: "8065857537235477817126228068",
+            },
+          ],
+        },
+      }
+    );
+
+    const response = await handleRequest(request, { jwtVerifySecret });
+    expect(fetch.mock.calls.length).toEqual(6);
+    expect(response.headers.get("Content-Type")).toBe("application/json");
+    expect(response.status).toBe(200);
+    expect(await response.json()).toEqual(
+      expect.objectContaining({
+        totalSupply: expect.any(String),
+        lockedSupply: expect.any(String),
+        lockedSupplyGenesis: expect.any(String),
+        liquidSupply: expect.any(String),
+        circulatingSupply: expect.any(String),
+      })
+    );
+  });
 });
--- src/utils/flow.ts
+++ src/utils/flow.ts
@@ -12,79 +12,86 @@ export async function handleRequest(
     jwtVerifySecret: string;
   }
 ): Promise<Response> {
-  const urlParams = new URL(request.url);
-  const params = Object.fromEntries(urlParams.searchParams);
-  const token = validateAndExtractTokenFromRequest(request);
+  try {
+    const urlParams = new URL(request.url);
+    const params = Object.fromEntries(urlParams.searchParams);
+    const token = validateAndExtractTokenFromRequest(request);
 
-  if (!token) {
-    return new Response(JSON.stringify({ error: "Missing Token" }), {
-      status: 400,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
+    if (!token) {
+      return new Response(JSON.stringify({ error: "Missing Token" }), {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
 
-  const isValid = await jwt.verify(token, options.jwtVerifySecret);
-  console.info(
-    `Authorization was trying to verify. The Authorization State: ${isValid}`
-  );
-  if (!isValid) {
-    return new Response(JSON.stringify({ error: "Unauthorized" }), {
-      status: 403,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
+    const isValid = await jwt.verify(token, options.jwtVerifySecret);
+    console.info(
+      `Authorization was trying to verify. The Authorization State: ${isValid}`
+    );
+    if (!isValid) {
+      return new Response(JSON.stringify({ error: "Unauthorized" }), {
+        status: 403,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
 
-  const timestamp = params.timestamp ? parseInt(params.timestamp) : null;
+    const timestamp = params.timestamp ? parseInt(params.timestamp) : null;
 
-  console.info(
-    `Params timestamp is: ${params.timestamp}, Timestamp for blockDetails: ${timestamp}`
-  );
-
-  if (!timestamp) {
-    const lastGlobalState = await getLatestGlobalState();
-
-    return new Response(JSON.stringify(lastGlobalState), {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } else {
-    const blockDetails = await getBlockByTimestamp(timestamp).then(
-      (blockInfo) => {
-        console.info(`blockDetails - blockInfo is:`, blockInfo);
-        if (!blockInfo) {
-          return getLatestBlock();
-        }
-
-        return blockInfo;
-      }
+    console.info(
+      `Params timestamp is: ${params.timestamp}, Timestamp for blockDetails: ${timestamp}`
     );
 
-    console.info(`blockDetails is:`, blockDetails);
+    if (!timestamp) {
+      const lastGlobalState = await getLatestGlobalState();
 
-    const globalStateDetails = await getGlobalStateByBlockNumber(
-      blockDetails
-    ).then((globalStateInfo) => {
-      console.info(`globalStateDetails - blockInfo is:`, globalStateInfo);
-      if (globalStateInfo == null) {
-        console.info(`globalStateInfo is missing:`, globalStateInfo);
+      return new Response(JSON.stringify(lastGlobalState), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } else {
+      const blockDetails = await getBlockByTimestamp(timestamp).then(
+        (blockInfo) => {
+          console.info(`blockDetails - blockInfo is:`, blockInfo);
+          if (!blockInfo) {
+            return getLatestBlock();
+          }
 
-        return getLatestGlobalState();
-      }
+          return blockInfo;
+        }
+      );
 
-      return globalStateInfo;
-    });
+      console.info(`blockDetails is:`, blockDetails);
 
-    console.info(`Global State is:`, globalStateDetails);
+      const globalStateDetails = await getGlobalStateByBlockNumber(
+        blockDetails
+      ).then((globalStateInfo) => {
+        console.info(`globalStateDetails - blockInfo is:`, globalStateInfo);
+        if (globalStateInfo == null) {
+          console.info(`globalStateInfo is missing:`, globalStateInfo);
 
-    return new Response(JSON.stringify(globalStateDetails), {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+          return getLatestGlobalState();
+        }
+
+        return globalStateInfo;
+      });
+
+      console.info(`Global State is:`, globalStateDetails);
+
+      return new Response(JSON.stringify(globalStateDetails), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error(
+      "Something went wrong - Please try again later or contact support"
+    );
   }
 }
