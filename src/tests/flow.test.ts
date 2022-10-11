28fb2853166a205cc42638405eca377ba5a4fad3
TuvalSimha <tuval.simha@gmail.com>
2022-10-11 10:09:03 +0300

    Add deploy + test

--- src/tests/flow.test.ts
+++ src/tests/flow.test.ts
@@ -23,9 +23,60 @@ describe("Request/Response flow", () => {
   beforeEach(() => {
     fetch.reset();
   });
-
   test("Should return a valid response when timestamp param is not valid", async () => {
-    expect.assertions(1);
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
+      {
+        data: {
+          blocks: [
+            {
+              id: "0xd125365fb2839beefb583319429a544da58758548a0454fddb9fed553ed94b06",
+              number: "15653542",
+              timestamp: "1664630075",
+            },
+          ],
+        },
+      }
+    );
+    expect(fetch.mock.calls[0][1]).toEqual(
+      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
+    );
+    const { request, jwtVerifySecret } = await buildValidRequest(
+      "?timestamp=!Ab3567"
+    );
+
+    mockFetch(
+      "POST",
+      "https://api.thegraph.com/subgraphs/name/juanmardefago/grt-circulating-supply",
+      {
+        data: {
+          globalStates: [
+            {
+              totalSupply: "10472593084278602817126230051",
+              lockedSupply: "2481516171545208333333335778",
+              lockedSupplyGenesis: "2406735547043125000000001983",
+              liquidSupply: "7991076912733394483792894273",
+              circulatingSupply: "8065857537235477817126228068",
+            },
+          ],
+        },
+      }
+    );
+
+    const response = await handleRequest(request, { jwtVerifySecret });
+    expect(fetch.mock.calls.length).toEqual(2);
+    expect(response.headers.get("Content-Type")).toBe("application/json");
+    expect(response.status).toBe(200);
+    expect(await response.json()).toEqual(
+      expect.objectContaining({
+        totalSupply: expect.any(String),
+        lockedSupply: expect.any(String),
+        lockedSupplyGenesis: expect.any(String),
+        liquidSupply: expect.any(String),
+        circulatingSupply: expect.any(String),
+      })
+    );
   });
 
   test("Should return a valid response when timestamp param is valid", async () => {
@@ -70,7 +121,7 @@ describe("Request/Response flow", () => {
     );
 
     const response = await handleRequest(request, { jwtVerifySecret });
-    expect(fetch.mock.calls.length).toEqual(2);
+    expect(fetch.mock.calls.length).toEqual(4);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
@@ -124,7 +175,7 @@ describe("Request/Response flow", () => {
     );
 
     const response = await handleRequest(request, { jwtVerifySecret });
-    expect(fetch.mock.calls.length).toEqual(4);
+    expect(fetch.mock.calls.length).toEqual(6);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
@@ -178,7 +229,7 @@ describe("Request/Response flow", () => {
     );
 
     const response = await handleRequest(request, { jwtVerifySecret });
-    expect(fetch.mock.calls.length).toEqual(6);
+    expect(fetch.mock.calls.length).toEqual(8);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
