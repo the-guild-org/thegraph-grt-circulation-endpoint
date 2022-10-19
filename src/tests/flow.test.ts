1856a9f053c9d7a084290bcd91442d161a15ffd5
Dotan Simha <dotansimha@gmail.com>
2022-10-19 21:10:46 +0900

    fix testing runtime

--- src/tests/flow.test.ts
+++ src/tests/flow.test.ts
@@ -1,8 +1,8 @@
 import { afterEach, beforeEach, describe, expect, test } from "@jest/globals";
 import { handleRequest } from "../utils/flow";
 import { sign } from "@tsndr/cloudflare-worker-jwt";
-import mockFetch from "jest-mock-fetch";
 import fetch from "jest-mock-fetch";
+import { mockFetch } from "./utils";
 
 async function buildValidRequest(queryParam: string) {
   const jwtVerifySecret = "fake";
@@ -20,9 +20,6 @@ async function buildValidRequest(queryParam: string) {
 }
 
 describe("Request/Response flow", () => {
-  beforeEach(() => {
-    fetch.reset();
-  });
   test("Should return a valid response when timestamp param is not valid", async () => {
     mockFetch(
       "POST",
@@ -38,9 +35,6 @@ describe("Request/Response flow", () => {
           ],
         },
       }
-    );
-    expect(fetch.mock.calls[0][1]).toEqual(
-      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
     );
     const { request, jwtVerifySecret } = await buildValidRequest(
       "?timestamp=!Ab3567"
@@ -65,12 +59,11 @@ describe("Request/Response flow", () => {
     );
 
     const response = await handleRequest(request, { jwtVerifySecret });
-    expect(fetch.mock.calls.length).toEqual(2);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
       expect.objectContaining({
-        totalSupply: expect.any(Number),
+        totalSupply: 10472593084.278602817126230051,
         lockedSupply: expect.any(Number),
         lockedSupplyGenesis: expect.any(Number),
         liquidSupply: expect.any(Number),
@@ -95,9 +88,6 @@ describe("Request/Response flow", () => {
         },
       }
     );
-    expect(fetch.mock.calls[0][1]).toEqual(
-      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
-    );
     const { request, jwtVerifySecret } = await buildValidRequest(
       `?timestamp=1665295732`
     );
@@ -109,7 +99,7 @@ describe("Request/Response flow", () => {
         data: {
           globalStates: [
             {
-              totalSupply: "10472593084278602817126230051",
+              totalSupply: "20472593084278602817126230051",
               lockedSupply: "2481516171545208333333335778",
               lockedSupplyGenesis: "2406735547043125000000001983",
               liquidSupply: "7991076912733394483792894273",
@@ -121,12 +111,11 @@ describe("Request/Response flow", () => {
     );
 
     const response = await handleRequest(request, { jwtVerifySecret });
-    expect(fetch.mock.calls.length).toEqual(4);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
       expect.objectContaining({
-        totalSupply: expect.any(Number),
+        totalSupply: 20472593084.278602817126230051,
         lockedSupply: expect.any(Number),
         lockedSupplyGenesis: expect.any(Number),
         liquidSupply: expect.any(Number),
@@ -151,9 +140,6 @@ describe("Request/Response flow", () => {
         },
       }
     );
-    expect(fetch.mock.calls[0][1]).toEqual(
-      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
-    );
     const { request, jwtVerifySecret } = await buildValidRequest("?timestamp=");
 
     mockFetch(
@@ -175,7 +161,6 @@ describe("Request/Response flow", () => {
     );
 
     const response = await handleRequest(request, { jwtVerifySecret });
-    expect(fetch.mock.calls.length).toEqual(6);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
@@ -205,9 +190,6 @@ describe("Request/Response flow", () => {
         },
       }
     );
-    expect(fetch.mock.calls[0][1]).toEqual(
-      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
-    );
     const { request, jwtVerifySecret } = await buildValidRequest("");
 
     mockFetch(
@@ -229,7 +211,6 @@ describe("Request/Response flow", () => {
     );
 
     const response = await handleRequest(request, { jwtVerifySecret });
-    expect(fetch.mock.calls.length).toEqual(8);
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
@@ -242,30 +223,11 @@ describe("Request/Response flow", () => {
       })
     );
   });
+
   test("When timestamp is not set -> Should return lastGlobalState value and divided", async () => {
     mockFetch(
       "POST",
-      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
-      {
-        data: {
-          blocks: [
-            {
-              id: "0xd125365fb2839beefb583319429a544da58758548a0454fddb9fed553ed94b06",
-              number: "15653542",
-              timestamp: "1664630075",
-            },
-          ],
-        },
-      }
-    );
-    expect(fetch.mock.calls[0][1]).toEqual(
-      "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks"
-    );
-    const { request, jwtVerifySecret } = await buildValidRequest("");
-
-    mockFetch(
-      "POST",
-      "https://api.thegraph.com/subgraphs/name/juanmardefago/grt-circulating-supply",
+      `https://api.thegraph.com/subgraphs/name/juanmardefago/grt-circulating-supply`,
       {
         data: {
           globalStates: [
@@ -281,16 +243,17 @@ describe("Request/Response flow", () => {
       }
     );
 
+    const { request, jwtVerifySecret } = await buildValidRequest("");
     const response = await handleRequest(request, { jwtVerifySecret });
     expect(response.headers.get("Content-Type")).toBe("application/json");
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(
       expect.objectContaining({
-        totalSupply: 1048929046.9016095,
-        lockedSupply: 228600236.02875,
-        lockedSupplyGenesis: 221181550.52264583,
-        liquidSupply: 820328810.8728596,
-        circulatingSupply: 827747496.3789637,
+        totalSupply: 10472593084.278602817126230051,
+        lockedSupply: 2481516171.545208333333335778,
+        lockedSupplyGenesis: 2406735547.043125000000001983,
+        liquidSupply: 7991076912.733394483792894273,
+        circulatingSupply: 8065857537.235477817126228068,
       })
     );
   });
